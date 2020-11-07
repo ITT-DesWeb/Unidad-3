89a8dc3e2058c737f328d7790a849e158461d7d3
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-11-06 16:18:42 -0800

    update pruebax

---
+++ Pruebax/Pruebax/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Pruebax
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
