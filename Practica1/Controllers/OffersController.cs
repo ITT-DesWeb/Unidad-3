5df301064dd663aecbbdfecfbf02d42ab77640d3
Juan Manuel Hernandez Martinez <juan.hernandezm@outlook.com>
2020-11-10 16:48:34 -0800

    Unidad Cmabios cafe

---
+++ Practica1/Controllers/OffersController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Practica1.Controllers
+{
+    public class OffersController : Controller
+    {
+        // GET: Offers
+        public ActionResult Index()
+        {
+            ViewBag.Nombre = "Este es mi ViewBag";
+            return View();
+        }
+    }
+}
